e2487142814543217ecaeecbb7bcec200319d866
keisuke.fukushima <kekke1102@gmail.com>
2018-09-27 12:06:02 +0000

    auth

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
-    @tasks = Task.order(created_at: :desc).all.page(params[:page]).per(10)
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(10)
   end
 
   def show
@@ -12,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task を登録しました'
