ae3d255796715a3f02e16431f6f49c4991a9024a
keisuke.fukushima <kekke1102@gmail.com>
2018-09-28 02:03:15 +0000

    correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(10)
   end
@@ -53,5 +55,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      flash[:danger] = 'タスクへの権限がありません。'
+      redirect_to root_url
+    end
   end
 end
